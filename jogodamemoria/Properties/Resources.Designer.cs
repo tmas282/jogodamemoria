bb0c0c9855e37eebe3eef3d1d49e182925bb2cb8
Ambidata VSTS <ambidata_vsts@ambidata.com>
2022-11-03 13:16:13 +0000

    mais bonito

--- jogodamemoria/Properties/Resources.Designer.cs
+++ jogodamemoria/Properties/Resources.Designer.cs
@@ -203,6 +203,26 @@ namespace jogodamemoria.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap imgBtnJogar {
+            get {
+                object obj = ResourceManager.GetObject("imgBtnJogar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap imgTitulo {
+            get {
+                object obj = ResourceManager.GetObject("imgTitulo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap sair {
             get {
                 object obj = ResourceManager.GetObject("sair", resourceCulture);
--- jogodamemoria/comecar.Designer.cs
+++ jogodamemoria/comecar.Designer.cs
@@ -31,11 +31,13 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(comecar));
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.btnJogar = new System.Windows.Forms.Button();
-            this.lblJogar = new System.Windows.Forms.Label();
             this.cbDificuldade = new System.Windows.Forms.ComboBox();
             this.btnSair = new System.Windows.Forms.PictureBox();
+            this.btnJogar = new System.Windows.Forms.PictureBox();
+            this.imgTitulo = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.btnSair)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnJogar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgTitulo)).BeginInit();
             this.SuspendLayout();
             // 
             // timer
@@ -43,41 +45,19 @@
             this.timer.Enabled = true;
             this.timer.Interval = 1000;
             // 
-            // btnJogar
-            // 
-            this.btnJogar.Location = new System.Drawing.Point(240, 220);
-            this.btnJogar.Margin = new System.Windows.Forms.Padding(2);
-            this.btnJogar.Name = "btnJogar";
-            this.btnJogar.Size = new System.Drawing.Size(102, 26);
-            this.btnJogar.TabIndex = 2;
-            this.btnJogar.Text = "Jogar";
-            this.btnJogar.UseVisualStyleBackColor = true;
-            this.btnJogar.Click += new System.EventHandler(this.BtnJogar_Click);
-            // 
-            // lblJogar
-            // 
-            this.lblJogar.AutoSize = true;
-            this.lblJogar.BackColor = System.Drawing.Color.Transparent;
-            this.lblJogar.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblJogar.Location = new System.Drawing.Point(58, 56);
-            this.lblJogar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblJogar.Name = "lblJogar";
-            this.lblJogar.Size = new System.Drawing.Size(477, 80);
-            this.lblJogar.TabIndex = 3;
-            this.lblJogar.Text = "Jogo da memória";
-            // 
             // cbDificuldade
             // 
             this.cbDificuldade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDificuldade.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbDificuldade.FormattingEnabled = true;
             this.cbDificuldade.Items.AddRange(new object[] {
             "Fácil",
             "Médio",
             "Difícil"});
-            this.cbDificuldade.Location = new System.Drawing.Point(197, 174);
+            this.cbDificuldade.Location = new System.Drawing.Point(158, 160);
             this.cbDificuldade.Margin = new System.Windows.Forms.Padding(2);
             this.cbDificuldade.Name = "cbDificuldade";
-            this.cbDificuldade.Size = new System.Drawing.Size(192, 21);
+            this.cbDificuldade.Size = new System.Drawing.Size(278, 39);
             this.cbDificuldade.TabIndex = 6;
             // 
             // btnSair
@@ -91,6 +71,29 @@
             this.btnSair.TabIndex = 7;
             this.btnSair.TabStop = false;
             // 
+            // btnJogar
+            // 
+            this.btnJogar.BackColor = System.Drawing.Color.Transparent;
+            this.btnJogar.BackgroundImage = global::jogodamemoria.Properties.Resources.imgBtnJogar;
+            this.btnJogar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnJogar.Location = new System.Drawing.Point(224, 215);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(148, 47);
+            this.btnJogar.TabIndex = 8;
+            this.btnJogar.TabStop = false;
+            this.btnJogar.Click += new System.EventHandler(this.BtnJogar_Click);
+            // 
+            // imgTitulo
+            // 
+            this.imgTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.imgTitulo.BackgroundImage = global::jogodamemoria.Properties.Resources.imgTitulo;
+            this.imgTitulo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgTitulo.Location = new System.Drawing.Point(67, 53);
+            this.imgTitulo.Name = "imgTitulo";
+            this.imgTitulo.Size = new System.Drawing.Size(465, 86);
+            this.imgTitulo.TabIndex = 9;
+            this.imgTitulo.TabStop = false;
+            // 
             // comecar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -98,10 +101,10 @@
             this.BackgroundImage = global::jogodamemoria.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.imgTitulo);
+            this.Controls.Add(this.btnJogar);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.cbDificuldade);
-            this.Controls.Add(this.lblJogar);
-            this.Controls.Add(this.btnJogar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -110,18 +113,19 @@
             this.Text = "Jogo da memória";
             this.Load += new System.EventHandler(this.Comecar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.btnSair)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnJogar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgTitulo)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.Button btnJogar;
-        private System.Windows.Forms.Label lblJogar;
         private System.Windows.Forms.ComboBox cbDificuldade;
         private System.Windows.Forms.PictureBox btnSair;
+        private System.Windows.Forms.PictureBox btnJogar;
+        private System.Windows.Forms.PictureBox imgTitulo;
     }
 }
 
